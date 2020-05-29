717af0b04fa88e2ab84e2b35be0701ff953c3359
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-29 15:12:11 -0700

    init the basic framework of flask
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
 from config import Config
-from flask_restx import Api
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
-
-api = Api(app)
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,4 @@
-from application import app, db, api
+from application import app
 from flask import (
     render_template,
     request,
@@ -8,14 +8,11 @@ from flask import (
     redirect,
     flash,
     url_for,
-    session,
 )
-from flask_restx import Resource
 
 
 @app.route("/", methods=["GET"])
 @app.route("/index", methods=["GET"])
 @app.route("/home", methods=["GET"])
 def index():
-    logged_in = session.get("email")
-    return render_template("index.html", index=True, logged_in=logged_in)
+    return render_template("index.html")
